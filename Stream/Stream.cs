63405bc933a129791c11e9da1cf5d4dc0ec4065b
Gholizadef1 <gholizadefatemeh37@gmail.com>
2022-01-23 10:16:20 +0330

    j

---
+++ Stream/Stream.cs
@@ -0,0 +1,20 @@
+using System.Collections.Generic;
+
+namespace Stream
+{
+    public class Stream : User:Chat
+    {
+        private string title { get; set; }
+    private User admin { get; set; }
+    private Time startTime { get; set; }
+    private long viewers { get; set; }
+    private long likeCount { get; set; }
+    private Chat chat { get; set; }
+
+    public long SendLike(User user)
+    {
+        likeCount = likeCount + 1;
+        return likeCount;
+    }
+}
+}
